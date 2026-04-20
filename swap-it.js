48159b48fd19bb946ad41bc8a46dd974f04a298b
Sune Simonsen <sune@we-knowhow.dk>
2026-04-20 10:56:01 +0200

    Support swapping multiple elements

--- swap-it.js
+++ swap-it.js
@@ -12,7 +12,15 @@ if (window.self === window.top) {
       const swap = url.searchParams.get('swap')
       url.searchParams.delete('swap')
 
-      document.getElementById(swap)?.replaceWith(iframe.contentDocument.getElementById(swap))
+      if (swap) {
+        for (const id of swap.split(',')) {
+          const currentElement = document.getElementById(id)
+          const updatedElement = iframe.contentDocument.getElementById(id)
+          if (currentElement && updatedElement) {
+            currentElement.replaceWith(updatedElement)
+          }
+        }
+      }
 
       history.replaceState(null, null, url.toString())
 
@@ -20,40 +28,59 @@ if (window.self === window.top) {
     })
   })
 
-  document.body.appendChild(iframe)
+  window.addEventListener('load', () => {
+    document.body.appendChild(iframe)
 
-  const withSwapParam = (href, swap) => {
-    const url = new URL(href)
-    url.searchParams.set('swap', swap)
-    return url.toString()
-  }
+    const withSwapParam = (href, swap) => {
+      const url = new URL(href)
+      url.searchParams.set('swap', swap)
+      return url.toString()
+    }
 
-  document.body.addEventListener('submit', (e) => {
-    if (e.target.matches('[data-swap]') || e.submitter.matches('[data-swap]')) {
-      const swap = e.submitter.getAttribute('data-swap') || e.target.getAttribute('data-swap')
-      const formAction = e.submitter.getAttribute('formaction')
-      const action = e.target.getAttribute('action')
-      const formTarget = e.submitter.getAttribute('formtarget')
-      const target = e.target.getAttribute('target')
+    document.body.addEventListener('submit', (e) => {
+      if (e.target.matches('[data-swap]') || e.submitter.matches('[data-swap]')) {
+        const swap = e.submitter.getAttribute('data-swap') || e.target.getAttribute('data-swap')
+        const formAction = e.submitter.getAttribute('formaction')
+        const action = e.target.getAttribute('action')
+        const formTarget = e.submitter.getAttribute('formtarget')
+        const target = e.target.getAttribute('target')
 
-      if (formAction) {
-        e.submitter.formAction = withSwapParam(e.submitter.formAction, swap)
-        setTimeout(() => {
-          e.submitter.setAttribute('formaction', formAction)
-        })
-      } else {
-        e.target.action = withSwapParam(e.target.action, swap)
-        setTimeout(() => {
-          e.target.setAttribute('action', action)
-        })
+        if (formAction) {
+          e.submitter.formAction = withSwapParam(e.submitter.formAction, swap)
+          setTimeout(() => {
+            e.submitter.setAttribute('formaction', formAction)
+          })
+        } else {
+          e.target.action = withSwapParam(e.target.action, swap)
+          setTimeout(() => {
+            e.target.setAttribute('action', action)
+          })
+        }
+
+        if (formTarget) {
+          e.submitter.setAttribute('formtarget', 'swap-it')
+          setTimeout(() => {
+            e.submitter.setAttribute('formtarget', formTarget)
+          })
+        } else {
+          e.target.setAttribute('target', 'swap-it')
+          setTimeout(() => {
+            if (target) {
+              e.target.setAttribute('target', target)
+            } else {
+              e.target.removeAttribute('target')
+            }
+          })
+        }
       }
+    })
 
-      if (formTarget) {
-        e.submitter.setAttribute('formtarget', 'swap-it')
-        setTimeout(() => {
-          e.submitter.setAttribute('formtarget', formTarget)
-        })
-      } else {
+    document.body.addEventListener('click', (e) => {
+      if (e.target.matches('a[data-swap]')) {
+        document.body.classList.add('loading')
+        const href = e.target.getAttribute('href')
+        e.target.href = withSwapParam(e.target.href, e.target.getAttribute('data-swap'))
+        const target = e.target.getAttribute('target')
         e.target.setAttribute('target', 'swap-it')
         setTimeout(() => {
           if (target) {
@@ -61,28 +88,9 @@ if (window.self === window.top) {
           } else {
             e.target.removeAttribute('target')
           }
+          e.target.setAttribute('href', href)
         })
       }
-    }
-  })
-
-  document.body.addEventListener('click', (e) => {
-    if (e.target.matches('a[data-swap]')) {
-      document.body.classList.add('loading')
-      const href = e.target.getAttribute('href')
-      e.target.href = withSwapParam(e.target.href, e.target.getAttribute('data-swap'))
-      const target = e.target.getAttribute('target')
-      e.target.setAttribute('target', 'swap-it')
-      setTimeout(() => {
-        if (target) {
-          e.target.setAttribute('target', target)
-        } else {
-          e.target.removeAttribute('target')
-        }
-        e.target.setAttribute('href', href)
-      })
-    }
+    })
   })
 }
-
-
